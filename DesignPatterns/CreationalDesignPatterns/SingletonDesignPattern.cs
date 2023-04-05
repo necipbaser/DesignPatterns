8e071db1125bcbb316bfbf4e62d6b0cb48500db0
necipbaser <necipbaser71@gmail.com>
2023-04-05 15:17:09 +0300

    singleton desing pattern

---
+++ DesignPatterns/CreationalDesignPatterns/SingletonDesignPattern.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalDesignPatterns
+{
+	public class SingletonDesignPattern
+	{
+		private static readonly object Instancelock = new object();
+
+		//eager loading
+		//private static SingletonDesignPattern singleton = new SingletonDesignPattern();
+		private static SingletonDesignPattern singleton;
+		private static int count = 0;
+		private SingletonDesignPattern()
+		{
+			Console.WriteLine($"created {count}");
+		}
+
+		public static SingletonDesignPattern getSingleton()
+		{
+			if (singleton == null)
+			{
+				//threadsafe control
+				lock (Instancelock)
+				{
+					if (singleton == null)
+					{
+						singleton = new SingletonDesignPattern();
+					}
+				}
+			}
+			count++;
+			return singleton;
+		}
+	}
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,10 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using DesignPatterns.CreationalDesignPatterns;
+
 internal class Program
 {
 	public static void Main(string[] args)
 	{
-        
-    }
+		//singleton
+		//SingletonDesignPattern singleton = SingletonDesignPattern.getSingleton();
+
+		for (int i = 0; i < 10; i++)
+		{
+			SingletonDesignPattern singleton = SingletonDesignPattern.getSingleton();
+		}
+	}
 }
